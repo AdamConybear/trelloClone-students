fe0dae73d700c63f0641509fa621eb8391ab7fb4
Adam Conybear <49500859+AdamConybear@users.noreply.github.com>
2020-05-22 22:37:15 -0400

    began adding CRUD options

--- src/actions/listActions.js
+++ src/actions/listActions.js
@@ -27,4 +27,24 @@ export const sort = (
             type
         }
     }
-}
+};
+
+export const editTitle = (listID, newTitle) => {
+    return {
+      type: CONSTANTS.EDIT_LIST_TITLE,
+      payload: {
+        listID,
+        newTitle
+      }
+    };
+};
+  
+export const deleteList = listID => {
+    return {
+      type: CONSTANTS.DELETE_LIST,
+      payload: {
+        listID
+      }
+    };
+};
+  
