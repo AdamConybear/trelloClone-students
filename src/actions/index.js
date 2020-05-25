822e05090475f658b34154867e4bc1d0187cee69
Adam Conybear <49500859+AdamConybear@users.noreply.github.com>
2020-05-25 19:08:45 -0400

    updated actions with classes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
-export * from "./listActions";
-export * from "./cardActions";
+export * from "./listsActions";
+export * from "./cardsActions";
 export * from "./classActions";
 
 export const CONSTANTS = {
-    ADD_CARD: "ADD_CARD",
-    ADD_LIST: "ADD_LIST",
-    DRAG_HAPPENED: "DRAG_HAPPENED",
-    EDIT_CARD: "EDIT_CARD",
-    DELETE_CARD: "DELETE_CARD",
-    EDIT_LIST_TITLE: "EDIT_LIST_TITLE",
-    DELETE_LIST: "DELETE_LIST",
-    SET_ACTIVE_CLASS: "SET_ACTIVE_CLASS",
-    ADD_CLASS: "ADD_CLASS",
-    DELETE_CLASS: "DELETE CLASS"
-}
+  ADD_CARD: "ADD_CARD",
+  ADD_LIST: "ADD_LIST",
+  DRAG_HAPPENED: "DRAG_HAPPENED",
+  EDIT_CARD: "EDIT_CARD",
+  DELETE_CARD: "DELETE_CARD",
+  EDIT_LIST_TITLE: "EDIT_LIST_TITLE",
+  DELETE_LIST: "DELETE_LIST",
+  SET_ACTIVE_CLASS: "SET_ACTIVE_CLASS",
+  ADD_CLASS: "ADD_CLASS",
+  DELETE_CLASS: "DELETE_CLASS",
+};
