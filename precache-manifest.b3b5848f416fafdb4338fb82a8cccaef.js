efb3d65a1eaf770fab93be577ddb2e56502c00be
Adam Conybear <49500859+AdamConybear@users.noreply.github.com>
2020-08-07 01:33:58 -0400

    Updates

---
+++ precache-manifest.b3b5848f416fafdb4338fb82a8cccaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4d14c83a8ed72e092f0",
+    "url": "/trelloClone-students/static/css/main.e8c933fe.chunk.css"
+  },
+  {
+    "revision": "c4d14c83a8ed72e092f0",
+    "url": "/trelloClone-students/static/js/main.c4d14c83.chunk.js"
+  },
+  {
+    "revision": "dd82b7e1a29074c4b535",
+    "url": "/trelloClone-students/static/js/runtime~main.dd82b7e1.js"
+  },
+  {
+    "revision": "7b160ef7e06f90a097cb",
+    "url": "/trelloClone-students/static/css/2.f5801497.chunk.css"
+  },
+  {
+    "revision": "7b160ef7e06f90a097cb",
+    "url": "/trelloClone-students/static/js/2.7b160ef7.chunk.js"
+  },
+  {
+    "revision": "2d1af0fc836d97b9fffc86fbb17b65d4",
+    "url": "/trelloClone-students/index.html"
+  }
+];
